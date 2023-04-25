9f83693a48d66356f24c61092b473768a4c52bf5
Kalu Wendy Chinwe <daintywhendee@yahoo.com>
2023-04-24 19:08:36 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * @void: No return value
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
